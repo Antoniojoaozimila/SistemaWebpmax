5964912ad742e6961dd9f4c2ec1b4d50468c722a
Antonio Joao Zimila <antoniojoaozimila@gmail.com>
2023-12-03 15:57:13 +0200

    SideBar e Nav

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import NavBar from '@/components/NavBar';
 import './globals.css';
 
 import SideBar from '@/components/SideBar';
@@ -5,12 +6,14 @@ import SideBar from '@/components/SideBar';
 export default function RootLayout({ children }) {
   return (
     <html lang="pt">
-      <body>
+      <body className='grid [grid-template-columns:250px_1fr] min-h-[100vh]'>
         <div>
         <SideBar/>
         </div>
 
-        <div></div>
+        <div>
+          <NavBar/>
+        </div>
       </body>
     </html>
   )
--- components/SideBar.js
+++ components/SideBar.js
@@ -15,29 +15,29 @@ export default function SideBar() {
   return (
     <aside
       className="bg-zinc-900 text-white
-    h-full p-4 w-[250px]"
+    h-full p-4 w-[250px] fixed"
     >
       <Link href="/">
         <Image
           src="/images/logo-sistema-web-pro-max.png"
           width={150}
           height={150}
+          className="block mx-auto"
         />
       </Link>
 
-      <nav>
+      <nav className="mt-8">
         <ul>
           {items.map((item, i) => {
             return (
               <li className="border-b border-dashed cursor-pointer">
-                <p>
-                    <span className="flex items-center gap-1">
-                    <FontAwesomeIcon icon={item.icon} className="w-6"/>
-                {item.name}
-                    </span>
-                    <FontAwesomeIcon icon={faChevronDown} className="w-6"/>
+                <p className="p-2 flex justify-between hover:bg-zinc-800">
+                  <span className="flex items-center gap-1">
+                    <FontAwesomeIcon icon={item.icon} className="w-6" />
+                    {item.name}
+                  </span>
+                  <FontAwesomeIcon icon={faChevronDown} className="w-4" />
                 </p>
-                
               </li>
             );
           })}
